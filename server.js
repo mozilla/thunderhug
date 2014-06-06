162f1c521a6971e7aacf9771e615579562b6d5f1
William Duyck <fuzzyfox0@gmail.com>
2014-06-06 21:14:57 +0100

    now w/ close to api spec output

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 // now for the app
 var express = require( 'express' );
 var morgan = require( 'morgan' );
+var moment = require( 'moment' );
+var marked = require( 'marked' );
 
 var shared = require( './shared' );
 var env = shared.env;
@@ -26,14 +28,96 @@ if( env.get( 'debug' ) || env.get( 'DEBUG' ) ) {
   app.use( morgan( 'dev' ) );
 }
 
+// return publicly safe proposals from full dataset
+function cleanProposals( proposals ) {
+  /**
+   * return twitter handle ONLY if allowed by user
+   */
+  function twitter( handle, proposal ) {
+    if( /twitter/ig.test( proposal.privacy ) ) {
+      return '';
+    }
+
+    return handle;
+  }
+
+  /**
+   * return organization ONLY if allowed by user
+   */
+  function organization( org, proposal ) {
+    if( /organization/ig.test( proposal.privacy ) ) {
+      return '';
+    }
+
+    return org;
+  }
+
+  var safeProposals = [];
+  proposals.forEach( function( proposal ) {
+    /*
+      Deal w/ session data
+     */
+    var safeProposal = {
+      title: proposal.title,
+      theme: proposal.theme,
+      organization: organization( proposal.organization, proposal ),
+      goals: marked( proposal.goals ),
+      agneda: marked( proposal.agenda ),
+      scale: marked( proposal.scale ),
+      outcomes: marked( proposal.outcomes )
+    };
+
+    // deal w/ timestamp
+    safeProposal.timestamp = moment( proposal.timestamp ).toISOString();
+
+    /*
+      deal w/ facilitators
+     */
+    safeProposal.facilitators = [];
+
+    // deal w/ first facilitator (required)
+    safeProposal.facilitators.push({
+      name: proposal.firstname + ' ' + proposal.surname,
+      twitter: twitter( proposal.twitter, proposal )
+    });
+
+    // deal w/ additional facilitators
+    var additionalFacilitators = proposal.facilitators.split( '\n' );
+    additionalFacilitators.forEach( function( facilitator ) {
+      var twitter = facilitator.match( /\B@[a-z0-9_-]+/i );
+      if( twitter ) {
+        twitter = twitter[ 0 ];
+      }
+      else {
+        twitter = '';
+      }
+
+      facilitator = facilitator.replace( /\B@[a-z0-9_-]+/gi, '' ).trim();
+
+      if( facilitator !== '' ) {
+        safeProposal.facilitators.push({
+          name: facilitator,
+          twitter: twitter
+        });
+      }
+    });
+
+    safeProposals.push( safeProposal );
+  });
+
+  return safeProposals;
+}
+
+// deal w/ api calls
 app.get( [ '/', '/healthcheck' ], function( request, response ) {
-  response.json( healthcheck );
+  response.jsonp( healthcheck );
 });
 
 app.get( '/all', function( request, response ) {
-  redisClient.get( 'thunderhug:megazord', function( error, data ) {
+  // get config
+  redisClient.get( 'thunderhug:meta', function( error, meta ) {
     if( error ) {
-      response.status( 500 ).json({
+      response.status( 500 ).jsonp({
         errors: [{
           message: 'unable to contact the database at this time',
           code: 500
@@ -43,10 +127,10 @@ app.get( '/all', function( request, response ) {
       return;
     }
 
-    if( data === null ) {
-      response.status( 500 ).json({
+    if( meta === null ) {
+      response.status( 500 ).jsonp({
         errors: [{
-          message: 'unable to get proposals at this time',
+          message: 'unable to get config information at this time',
           code: 500
         }]
       });
@@ -54,16 +138,122 @@ app.get( '/all', function( request, response ) {
       return;
     }
 
-    data = JSON.parse( data );
-    var proposals = data.data;
+    meta = JSON.parse( meta );
 
-    response.json( proposals );
+    // get megazord (all) proposals
+    redisClient.get( 'thunderhug:megazord', function( error, data ) {
+      if( error ) {
+        response.status( 500 ).jsonp({
+          errors: [{
+            message: 'unable to contact the database at this time',
+            code: 500
+          }]
+        });
+        console.log( error );
+        return;
+      }
+
+      if( data === null ) {
+        response.status( 500 ).jsonp({
+          errors: [{
+            message: 'unable to get proposals at this time',
+            code: 500
+          }]
+        });
+        console.error( 'unable to get data for the megazord sheet' );
+        return;
+      }
+
+      data = JSON.parse( data );
+      var proposals = data.data;
+      var safeProposals = cleanProposals( proposals );
+
+      response.jsonp( safeProposals );
+    });
   });
 });
 
-app.get( '/theme/:theme', function( request, response ) {
-  // something here
-  response.json( {} );
+app.get( '/:theme', function( request, response ) {
+  redisClient.get( 'thunderhug:meta', function( error, meta ) {
+    if( error ) {
+      response.status( 500 ).jsonp({
+        errors: [{
+          message: 'unable to contact the database at this time',
+          code: 500
+        }]
+      });
+      console.log( error );
+      return;
+    }
+
+    if( meta === null ) {
+      response.status( 500 ).jsonp({
+        errors: [{
+          message: 'unable to get config information at this time',
+          code: 500
+        }]
+      });
+      console.error( 'unable to get data for the megazord sheet' );
+      return;
+    }
+
+    meta = JSON.parse( meta );
+    var themeName = '';
+    meta.data.forEach( function( item ) {
+      if( item.type === 'theme' && item.key === request.params.theme ) {
+        themeName = item.value;
+      }
+    });
+
+    if( themeName === '' ) {
+      response.status( 404 ).jsonp({
+        errors: [{
+          message: 'the theme you requested was not recognized',
+          code: 404
+        }]
+      });
+      return;
+    }
+
+    // get megazord (all) proposals
+    redisClient.get( 'thunderhug:megazord', function( error, data ) {
+      if( error ) {
+        response.status( 500 ).jsonp({
+          errors: [{
+            message: 'unable to contact the database at this time',
+            code: 500
+          }]
+        });
+        console.log( error );
+        return;
+      }
+
+      if( data === null ) {
+        response.status( 500 ).jsonp({
+          errors: [{
+            message: 'unable to get proposals at this time',
+            code: 500
+          }]
+        });
+        console.error( 'unable to get data for the megazord sheet' );
+        return;
+      }
+
+      data = JSON.parse( data );
+      var proposals = data.data;
+      var themeProposals = [];
+
+      proposals.forEach( function( proposal ) {
+        if( proposal.theme === themeName ) {
+          themeProposals.push( proposal );
+        }
+      });
+
+      var safeProposals = cleanProposals( themeProposals );
+
+      response.jsonp( safeProposals );
+    });
+  });
 });
 
 // launch the server
